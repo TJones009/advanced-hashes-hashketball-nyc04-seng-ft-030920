9b3b45688da2432472a7e72d7a2610f18310fbec
Trevor Jones <tjones009@yahoo.com>
2020-03-05 19:02:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,12 +165,13 @@ def team_names
 end
 
 def player_numbers(team_name)
-  game_hash.map do |place, team|
+  nums =[]
+  game_hash.each do |place, team|
     if team[:team_name] == team_name
       team.each do |attributes, data|
         if attributes == :players
           data.each do |player|
-            player[:number]
+            nums.push(player[:number])
       end
     end
   end

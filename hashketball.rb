1964c2f3ca03bd3c2ee538de0cd4ea842b616788
Trevor Jones <tjones009@yahoo.com>
2020-03-05 20:57:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -259,9 +259,8 @@ def player_with_longest_name
       name_length = player[:player_name].length
       longest_name, longest_length = player[:player_name], name_length if name_length > longest_length
     end
+  end
   return longest_name
-  
-  
 end
 
 
